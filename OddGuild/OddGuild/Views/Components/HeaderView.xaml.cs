9c58d45da93f6ffd6171407e57a73bf733ceeff6
justrayn <vonraynmal@gmail.com>
2026-05-19 19:16:16 +0800

    added blank pages

---
+++ OddGuild/OddGuild/Views/Components/HeaderView.xaml.cs
@@ -0,0 +1,9 @@
+namespace OddGuild.Views.Components;
+
+public partial class HeaderView : ContentView
+{
+    public HeaderView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OddGuild/OddGuild/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace OddGuild.Views;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
